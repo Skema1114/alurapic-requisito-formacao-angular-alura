e63359ff507c9d97e74e53e75ee8eb9bfbdeb8fa
Rafael Aranda Martins <skema1114@hotmail.com>
2021-06-02 15:49:39 -0300

    Iniciando o curso

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,11 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'alurapic-requisito-formacao-angular-alura';
+  src =
+    'https://img.ibxk.com.br/2016/02/24/24120655953583.jpg?w=1120&h=420&mode=crop&scale=both';
+  alt = 'windows';
 }
