3969ccd0e9462073d74cdbd3226cc2709032d012
Rafael Aranda Martins <skema1114@hotmail.com>
2021-06-10 15:26:55 -0300

    Terminando a parte 03

--- alurapic/src/app/core/core.module.ts
+++ alurapic/src/app/core/core.module.ts
@@ -7,9 +7,10 @@ import { HeaderComponent } from './header/header.component';
 import { RequestInterceptor } from './auth/request.interceptor';
 import { FooterComponent } from './footer/footer.component';
 import { AlertModule } from '../shared/components/alert/alert.module';
+import { LoadingModule } from '../shared/components/loading/loading.module';
 
 @NgModule({
-  imports: [CommonModule, RouterModule, AlertModule],
+  imports: [CommonModule, RouterModule, AlertModule, LoadingModule],
   declarations: [HeaderComponent, FooterComponent],
   exports: [HeaderComponent, FooterComponent],
   providers: [
--- alurapic/src/app/shared/components/vmessage/vmessage.module.ts
+++ alurapic/src/app/shared/components/vmessage/vmessage.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
 import { VmessageComponent } from './vmessage.component';
 
 @NgModule({
